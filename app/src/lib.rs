6adefea5a394b75d2d51817e8a72781f34e62e2e
aleokdev <aleok.inf@gmail.com>
2023-02-04 13:46:17 +0100

    Fix movement speed

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -104,41 +104,6 @@ pub fn run(width: u32, height: u32) -> anyhow::Result<()> {
         let Some(app) = application.as_mut() else { return };
         *control_flow = ControlFlow::Poll;
 
-        let now_time = std::time::Instant::now();
-        let delta = now_time - last_time;
-        let delta_s = delta.as_secs_f32();
-        const SPEED: f32 = 100.;
-
-        let player_pos = app.world.camera.position();
-        let player_pos = ivec3(
-            player_pos.x.floor() as i32,
-            player_pos.y.floor() as i32,
-            (player_pos.z / CEILING_HEIGHT).floor() as i32,
-        );
-        if let Some(id) = app
-            .database
-            .map
-            .floor(player_pos.z)
-            .and_then(|floor| floor.cell(player_pos.truncate()))
-        {
-            let current_room = &app.database.rooms[id];
-            let current_node = &app.database.nodes[current_room.node()];
-            // Load two children into the node tree
-            let nodes_to_load = current_node.children.iter().copied().flat_map(|node_idx| {
-                std::iter::once(node_idx)
-                    .chain(app.database.nodes[node_idx].children.iter().copied())
-            });
-            let rooms_to_load = nodes_to_load
-                .flat_map(|node| app.database.nodes[node].rooms.iter())
-                .filter(|room| !app.room_meshes_uploaded.contains(&room))
-                .collect::<Vec<_>>();
-            for &room_id in rooms_to_load {
-                app.renderer
-                    .upload_mesh(meshgen::generate_room_mesh(&app.database, room_id));
-                app.room_meshes_uploaded.insert(room_id);
-            }
-        }
-
         match event {
             Event::WindowEvent { event, .. } => match event {
                 WindowEvent::Resized(new_size) => unsafe {
@@ -227,8 +192,45 @@ pub fn run(width: u32, height: u32) -> anyhow::Result<()> {
                 }
                 _ => (),
             },
-            Event::MainEventsCleared => app.cx.window.request_redraw(),
+            Event::MainEventsCleared => {
+                let player_pos = app.world.camera.position();
+                let player_pos = ivec3(
+                    player_pos.x.floor() as i32,
+                    player_pos.y.floor() as i32,
+                    (player_pos.z / CEILING_HEIGHT).floor() as i32,
+                );
+                if let Some(id) = app
+                    .database
+                    .map
+                    .floor(player_pos.z)
+                    .and_then(|floor| floor.cell(player_pos.truncate()))
+                {
+                    let current_room = &app.database.rooms[id];
+                    let current_node = &app.database.nodes[current_room.node()];
+                    // Load two children into the node tree
+                    let nodes_to_load =
+                        current_node.children.iter().copied().flat_map(|node_idx| {
+                            std::iter::once(node_idx)
+                                .chain(app.database.nodes[node_idx].children.iter().copied())
+                        });
+                    let rooms_to_load = nodes_to_load
+                        .flat_map(|node| app.database.nodes[node].rooms.iter())
+                        .filter(|room| !app.room_meshes_uploaded.contains(&room))
+                        .collect::<Vec<_>>();
+                    for &room_id in rooms_to_load {
+                        app.renderer
+                            .upload_mesh(meshgen::generate_room_mesh(&app.database, room_id));
+                        app.room_meshes_uploaded.insert(room_id);
+                    }
+                }
+                app.cx.window.request_redraw();
+            }
             Event::RedrawRequested(_) => unsafe {
+                let now_time = std::time::Instant::now();
+                let delta = now_time - last_time;
+                let delta_s = delta.as_secs_f32();
+                const SPEED: f32 = 5.;
+
                 let mut movement = Vec3::ZERO;
                 movement.z += if movement_keys_pressed[0] { 1. } else { 0. };
                 movement.z -= if movement_keys_pressed[1] { 1. } else { 0. };
@@ -243,6 +245,8 @@ pub fn run(width: u32, height: u32) -> anyhow::Result<()> {
                     .camera
                     .move_local_coords(movement * SPEED * delta_s);
                 app.redraw(delta).unwrap();
+
+                last_time = now_time;
             },
             Event::LoopDestroyed => unsafe {
                 let mut app = application.take().unwrap();
@@ -250,7 +254,5 @@ pub fn run(width: u32, height: u32) -> anyhow::Result<()> {
             },
             _ => {}
         }
-
-        last_time = now_time;
     })
 }
